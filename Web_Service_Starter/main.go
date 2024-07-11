44582d6f46aa77d211cf96482fca62695bc621e1
Supachot <supachot.4762@gmail.com>
2024-07-11 11:36:29 +0700

    [Updated] add comments

--- Web_Service_Starter/main.go
+++ Web_Service_Starter/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// function for log request to show in sonsole
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("[%s] %s %s\n", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL)
@@ -13,6 +14,7 @@ func loggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+// function called Hello World
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
