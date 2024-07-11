e9f8f7f8fc6b99519f71ab59f2373f8c15602395
Supachot <supachot.4762@gmail.com>
2024-07-11 23:29:13 +0700

    [Edited] Create first endpoint with fiber

--- Go_Todo/main.go
+++ Go_Todo/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
 	fmt.Println("App running")
+	app := fiber.New()
+	app.Use(cors.New())
+
+	api := app.Group("/api")
+
+	// Test handler
+	api.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("App running")
+	})
+
+	log.Fatal(app.Listen(":8080"))
 }
