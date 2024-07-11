330cc68e119cbab460be8d000e3203f520bd885b
Supachot <supachot.4762@gmail.com>
2024-07-11 11:34:12 +0700

    [Added] Aded new function log request to show in console

--- Web_Service_Starter/main.go
+++ Web_Service_Starter/main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("[%s] %s %s\n", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -11,9 +19,9 @@ func main() {
 	})
 
 	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-        name := r.URL.Path[len("/greet/"):]
-        fmt.Fprintf(w, "Hello, %s!", name)
-    })
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello, %s!", name)
+	})
 
 	http.ListenAndServe(":8081", nil)
 }
