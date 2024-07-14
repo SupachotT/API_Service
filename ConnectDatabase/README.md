87ee3ec5203eb727fef0b20bea4007e03504f86b
Supachot <supachot.4762@gmail.com>
2024-07-14 15:00:38 +0700

    [Created] create database server, connect to database, create table Customers and, insert data to database

---
+++ ConnectDatabase/README.md
@@ -0,0 +1,29 @@
+# DATABEASES CONNECTION
+This project is for create database from Image Docker "postgres" and connect postgres to Go Language
+Reference form **[This](https://youtu.be/Y7a0sNKdoQk?si=j9jtPJX2drl7oB90)**.
+
+## Create Database server
+1. Pull Image Postgres from Docker hub.
+
+    ```
+    docker pull postgres
+    ```
+2. Run docker with command
+   
+    ```
+    docker run --name pg-container -e POSTGRES_PASSWORD=secret -p 5432:5432 -d postgres
+    ```
+3. Create Database server and connect to server
+
+    ```
+    docker -ti pg-container createdb -U postgres gopgtest    
+    ```
+    After that connect to database server with
+
+    ```
+    docker exec -ti pg-container psql -U postgres
+    ```
+
+4. Others
+
+    for check that it connected to database with sql command  `\c` and `\q` for exit from databases
---
+++ ConnectDatabase/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Customer struct {
+	First_name   string
+	Last_name    string
+	Address      string
+	District     string
+	Sub_district string
+	City         string
+	Postal_code  string
+	Phone        string
+	Email        string
+}
+
+func main() {
+	connStr := "postgres://postgres:secret@localhost:5432/gopgtest?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	createLoanCustomerTable(db)
+
+	customer := Customer{"Benjamin", "Franklin", "132/1 Gp 2 Vongvaenrobnok Tavee Watana Thawi Watthana", "Phen", "Sumsao", "Udon Thani", "71150", "034-591201", "admin@loomsoom.go.th"}
+	pk := InsertLoanCustomer(db, customer)
+
+	fmt.Printf("ID = %d\n", pk)
+}
+
+func createLoanCustomerTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS Customers (
+		customer_id SERIAL PRIMARY KEY,
+		first_name VARCHAR(50) NOT NULL,
+		last_name VARCHAR(50) NOT NULL,
+		address VARCHAR(255) NOT NULL,
+		district VARCHAR(50) NOT NULL,
+		sub_district VARCHAR(50) NOT NULL,
+		city VARCHAR(50) NOT NULL,
+		postal_code VARCHAR(20) NOT NULL,
+		phone VARCHAR(15) NOT NULL,
+		email VARCHAR(100) NOT NULL UNIQUE,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func InsertLoanCustomer(db *sql.DB, customer Customer) int {
+	query := `INSERT INTO Customers (first_name, last_name, address, district, sub_district, city, postal_code, phone, email)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING customer_id`
+
+	var pk int
+	err := db.QueryRow(query, customer.First_name, customer.Last_name, customer.Address, customer.District, customer.Sub_district, customer.City, customer.Postal_code, customer.Phone, customer.Email).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pk
+
+}
